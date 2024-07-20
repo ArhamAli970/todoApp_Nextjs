e47d3b74f635eafbfee3303c74b918380196e0fe
Arham <arhamali0012000@gmail.com>
2024-07-20 12:28:10 +0530

    chg

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import Navbar from "./navbar";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
